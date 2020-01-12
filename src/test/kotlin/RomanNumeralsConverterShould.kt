fd748417a3ea66ec318ac0e66ce68f6b2d8e9007
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-01-12 10:59:34 +0000

    Refactored test to parameterised tests to remove duplication

--- src/test/kotlin/RomanNumeralsConverterShould.kt
+++ src/test/kotlin/RomanNumeralsConverterShould.kt
@@ -1,16 +1,22 @@
-import org.amshove.kluent.`should be`
-import org.junit.jupiter.api.Test
+import org.amshove.kluent.`should equal`
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class RomanNumeralsConverterShould {
-    @Test
-    fun `convert number one to roman numeral I`() {
-        val converter = RomanNumeralConverter()
-        converter.convert(1) `should be` "I"
+    private lateinit var converter: RomanNumeralConverter
+
+    @BeforeEach
+    fun setUp() {
+        converter = RomanNumeralConverter()
     }
 
-    @Test
-    fun `convert number two to roman numeral II`() {
-        val converter = RomanNumeralConverter()
-        converter.convert(2) `should be` "II"
+    @ParameterizedTest
+    @CsvSource(
+        "1, I",
+        "2, II"
+    )
+    fun `convert number to roman numeral`(number: Int, numeral: String) {
+        converter.convert(number) `should equal` numeral
     }
 }
